47dc95186b93738d186b79568ca1d97c3cb04921
shivamrajput1826 <rajputshiv1610@gmail.com>
2022-10-26 01:01:12 +0530

    env vartiable name changed

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 module.exports = async function() {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.API_KEY}&pageSize=5`);
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
     return response.data;
   } catch (error) {
     console.error(error);
